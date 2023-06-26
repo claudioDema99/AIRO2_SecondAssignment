a24ecef37f5c60cb98083917394558ced211bf28
Gianluca Galvagni <gianluca.galvagni99@gmail.com>
2023-06-26 21:51:37 +0200

    Update VisitSolver.h

--- visits_module/src/VisitSolver.h
+++ visits_module/src/VisitSolver.h
@@ -50,10 +50,13 @@ public:
     vector <string> source, target;
     string starting_position;
     int k;
-     
 
+    double euclideanDist(string from, string to);
+    string wp[30] = {"wp0", "wp1", "wp2", "wp3", "wp4", "wp5", "wp6", "wp7", "wp8", "wp9", "wp10", "wp11", "wp12", "wp13", "wp14", "wp15", "wp16", "wp17", "wp18", "wp19", "wp20", "wp21", "wp22", "wp23", "wp24", "wp25", "wp26", "wp27", "wp28", "wp29"};
+    string reg[30] = {"r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7","r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15","r16", "r17", "r18", "r19", "r20", "r21", "r22", "r23","r24", "r25", "r26", "r27", "r28", "r29"};
+    double distance;
+    
     void parseParameters(string parameters);
-    double distance_euc(string from, string to);
  
 
 private:
